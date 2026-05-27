7490df976b0ac4591a85cfaadb9d539ad8436131
MinhTan210905 <tan210905@gmail.com>
2026-05-28 03:40:47 +0700

    clean: Remove temporary test files and helper scripts used during debugging and optimization

--- update_controllers.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dir = 'd:/Student_HCMUE/PHP/htdocs/PHP_CodeIgniter_TradingDocument/application/controllers/';
-foreach(glob($dir.'*.php') as $f) {
-    file_put_contents($f, str_replace('count_pending_for_seller', 'count_action_required', file_get_contents($f)));
-}
-echo "Done";
